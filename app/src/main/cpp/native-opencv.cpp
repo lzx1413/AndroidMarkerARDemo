9c0eb8be2eb56939ab0be42d753d71c0a91e3df9
lzx1413 <lzx1413@live.cn>
2017-05-13 21:39:52 +0800

    init homework

--- app/src/main/cpp/native-opencv.cpp
+++ app/src/main/cpp/native-opencv.cpp
@@ -2,15 +2,10 @@
 // Created by Huang on 2017/3/4.
 //
 
+#include <vector>
 #include <jni.h>
 #include <opencv2/opencv.hpp>
-#include <vector>
 //caffe2
-#include <caffe2/core/predictor.h>
-#include <caffe2/core/operator.h>
-#include <caffe2/core/timer.h>
-#include "caffe2/core/init.h"
-//#include <caffe2/utils/proto_utils.h>
 #include <android/asset_manager.h>
 #include <android/asset_manager_jni.h>
 #include <android/log.h>
@@ -18,22 +13,6 @@
 #define alog(...) __android_log_print(ANDROID_LOG_ERROR, "F8DEMO", __VA_ARGS__);
 using namespace cv;
 using namespace std;
-static caffe2::NetDef _initNet, _predictNet;
-static caffe2::Predictor* _predictor;
-static caffe2::Workspace ws;
-
-void loadToNetDef(AAssetManager* mgr,caffe2::NetDef* net, const char* filename){
-    AAsset* asset = AAssetManager_open(mgr, filename, AASSET_MODE_BUFFER);
-    assert(asset != nullptr);
-    const void *data = AAsset_getBuffer(asset);
-    assert(data != nullptr);
-    off_t len = AAsset_getLength(asset);
-    assert(len != 0);
-    if (!net->ParseFromArray(data, len)) {
-        alog("Couldn't parse net from data.\n");
-    }
-    AAsset_close(asset);
-}
 extern "C" {
 void Java_com_huang_opencvtest_MainActivity_nativeProcessFrame(JNIEnv *env, jobject thiz, jlong addrGray, jlong addrRGBA){
     Mat& gray = *(Mat *) addrGray;
